fc68eaed6d3292edb374189662aeb51c2f8a7bde
anders <anders@columbia.edu>
2011-12-21 23:49:20 -0500

    not where it should be

--- src/controller/tailserver_tail_controller.erl
+++ src/controller/tailserver_tail_controller.erl
@@ -2,4 +2,12 @@
 -compile(export_all).
 
 hello('GET', []) ->
-    {output,"Hello World"}.
+    TimeStamp = boss_mq:now("tail-channel"),
+    {ok,[{timestamp, TimeStamp}]}.
+
+pull('GET', [LastTimeStamp]) ->
+    {ok, Timestamp, Lines} = boss_mq:pull("tail-channel", list_to_integer(LastTimeStamp)),
+    io:format("got ~p~n",[Lines]),
+    {json, [{timestamp, Timestamp}, {lines, Lines}]}.
+
+
