320907f4e6993fb3aa90513f437df4de773695c9
anders <anders@columbia.edu>
2011-12-22 00:09:22 -0500

    cleaning up loop a bit

--- src/lib/tailf.erl
+++ src/lib/tailf.erl
@@ -20,26 +20,20 @@ info_display(Content) ->
 
 loop(Port,Callback) ->
     receive
-        {Port, {data, {eol, Bin}}} ->
+        {Port, {data, {_EolFlag, Bin}}} ->
             Content = iolist_to_binary(Bin),
 	    Callback(Content),
             loop(Port,Callback);
-%%        {Port, {exit_status, Status}} ->
-%%	    io:format("got exit_status message ~p~n",[Status]),
-%%            {ok, Status};
-%%        {Port, eof} -> 
-%%	    io:format("got eof message~n"),
-%%            port_close(Port),
-%%            {ok, eof};
+        {Port, {exit_status, Status}} ->
+            {ok, Status};
+        {Port, eof} -> 
+            port_close(Port),
+            {ok, eof};
         stop ->
-            Port ! {self(), close},
-            receive
-                {Port, closed} ->
-                    exit(normal)
-            end
-%%        {'EXIT',Port,Reason} ->
-%%	    io:format("terminated ~p~n",[Reason]),
-%%            exit(port_terminated);
-%%	_ANY_ ->
-%%	    io:format("got something unexpected~n")
+	    port_close(Port),
+	    {ok, stop};
+        {'EXIT',Port,Reason} ->
+            exit(port_terminated);
+	_ANY ->
+	    loop(Port,Callback)
     end.
